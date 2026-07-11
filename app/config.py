d4f8fa53285774d4906ffa234b3f9114a1bf9460
Guillermo Courtade <guillermocourtademorales@gmail.com>
2026-07-10 21:50:32 -0600

    Connect OpenAI API

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ load_dotenv()  # Busca automáticamente un archivo .env en el directorio actual 
 APP_NAME = os.getenv("APP_NAME")
 APP_VERSION= os.getenv("APP_VERSION")
 APP_ENV= os.getenv("APP_ENV")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.config import APP_NAME, APP_VERSION, APP_ENV
-from Models.message import Message
+from models.message import Message
+from services.openai_service import generate_response
 
 app = FastAPI()
 
@@ -22,5 +23,5 @@ def about():
     return {"message": "This is an AI Document Assistant API built with FastAPI."}
 
 @app.post("/chat")
-def chat(message: Message):
-    return {"received": message.message}
+def chat_endpoint(message: Message):
+    return {"received": generate_response(message.message)}
