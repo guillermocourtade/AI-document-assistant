cf9b0fd6ab670724ac62e94612301bc1146f3c71
Guillermo Courtade <guillermocourtademorales@gmail.com>
2026-07-12 12:21:17 -0600

    Extract text and get chunks

--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from app.services.document_service import validate_pdf
+from app.services.document_service import validate_pdf, extract_text_from_pdf, split_text
 
 router = APIRouter()
 
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     validate_pdf(file)
+    text = extract_text_from_pdf(file)
+    chunks = split_text(text)
     return {"filename": file.filename, 
-            "content_type": file.content_type}
+            "num_chunks": len(chunks),
+            "chunks": chunks}
--- app/services/document_service.py
+++ app/services/document_service.py
@@ -1,4 +1,6 @@
 from fastapi import HTTPException
+from pypdf import PdfReader
+from io import BytesIO
 
 def validate_pdf(file):
     if file.content_type != "application/pdf":
@@ -6,3 +8,26 @@ def validate_pdf(file):
             status_code=400,
             detail="Only PDF files are allowed."
         )
+    
+
+def extract_text_from_pdf(file):
+    pdf = PdfReader(BytesIO(file.file.read()))
+
+    text = ""
+
+    for page in pdf.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text + "\n"
+
+    return text
+
+def split_text(text, chunk_size=500):
+    chunks = []
+
+    for i in range(0, len(text), chunk_size):
+        chunk = text[i:i + chunk_size]
+        chunks.append(chunk)
+
+    return chunks
